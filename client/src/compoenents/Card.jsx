4542378bc3df350c1d3e36ba9729ae76b4eec7a5
IT21826740 <IT21826740@my.sliit.lk>
2024-06-15 17:25:53 +0530

    Card.jsx

---
+++ client/src/compoenents/Card.jsx
@@ -0,0 +1,12 @@
+
+const Card = ({ children}) => {
+  
+  return (
+    <div className="p-4 bg-white shadow-md shadow-gray-300 rounded-md mb-5">
+      {children}
+    </div>
+  );
+}
+
+export default Card
+
