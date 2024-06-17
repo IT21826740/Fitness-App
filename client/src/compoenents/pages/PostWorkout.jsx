d7a83767d26f66bdcee876fbf3c68f24ece25ba8
IT21826740 <IT21826740@my.sliit.lk>
2024-06-17 16:00:48 +0530

    Create PostWorkout.jsx

---
+++ client/src/compoenents/pages/PostWorkout.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Layout from '../Layout'
+
+function PostWorkout() {
+  return (
+    <div>
+        <Layout>
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </Layout>
+
+    </div>
+  )
+}
+
+export default PostWorkout
