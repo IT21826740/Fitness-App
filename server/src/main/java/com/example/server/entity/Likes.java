dc57e2dc1dc127865017f66046a63e9a979c9f19
IT21826740 <IT21826740@my.sliit.lk>
2024-06-18 23:13:41 +0530

    Create likes entity

---
+++ server/src/main/java/com/example/server/entity/Likes.java
@@ -0,0 +1,68 @@
+package com.example.server.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(
+    name = "likes",
+        uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id","meal_id"})
+    }
+)
+public class Likes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "meal_id")
+    private MealPlan mealPlan;
+ 
+    public Likes() {
+
+    }
+
+    public Likes(User user, MealPlan mealPlan) {
+        this.user = user;
+        this.mealPlan = mealPlan;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public MealPlan getMealPlan() {
+        return mealPlan;
+    }
+
+    public void setMealPlan(MealPlan mealPlan) {
+        this.mealPlan = mealPlan;
+    }
+    
+
+    
+}
